d0f08751e3d2e1507e29f69e8a0cc317a64f98c1
David Evans <david0evans@gmail.com>
2018-01-27 22:57:52 -0800

    tweak lighting

--- ggj-2018/Assets/Game/Scripts/Lighting.cs
+++ ggj-2018/Assets/Game/Scripts/Lighting.cs
@@ -7,14 +7,19 @@ public class Lighting : MonoBehaviour
   private Light _sunLight = null;
 
   [SerializeField]
+  private Light _secondaryLight = null;
+
+  [SerializeField]
   private Color _lightingTint = Color.white;
 
   private void Update()
   {
     if (_sunLight != null)
     {
-      Shader.SetGlobalVector("_SkyLightDir", _sunLight.transform.forward);
-      Shader.SetGlobalColor("_SkyLightColor", _sunLight.color);
+      Shader.SetGlobalVector("_SkyLightDir0", _sunLight.transform.forward);
+      Shader.SetGlobalColor("_SkyLightColor0", _sunLight.color);
+      Shader.SetGlobalVector("_SkyLightDir0", _secondaryLight.transform.forward);
+      Shader.SetGlobalColor("_SkyLightColor0", _secondaryLight.color);
       Shader.SetGlobalColor("_TimeOfDayTint", _lightingTint);
     }
   }
