c18e38aa803b4af279ca4d9c23bedd8f1128e3d7
David Evans <david0evans@gmail.com>
2018-01-28 17:45:55 -0800

    whatever these last changes were

---
+++ push-itch.sh
@@ -0,0 +1,3 @@
+butler push build/no-monster-win/ stickybiscuits/ggj2018:windows
+butler push build/no-monster-mac/ stickybiscuits/ggj2018:osx
+butler push build/no-monster-linux/ stickybiscuits/ggj2018:linux
